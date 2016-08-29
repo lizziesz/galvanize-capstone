dc2d093cc2611f76fcf50dab1e0a6398417fe7a0
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-08-29 13:40:22 -0600

    Added food options to search form

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,6 +3,12 @@ var app = angular.module('decisionApp', ['ngRoute']);
 app.controller("DecisionController", ['$scope', function($scope) {
   $scope.view = {};
   $scope.view.inputLocation = true;
+
+  $scope.view.changeInput = function(){
+    $scope.view.inputLocation = false;
+    $scope.view.inputTypeOfFood = true;
+  }
+  
 }]);
 
 app.config(function($routeProvider){
--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -3,4 +3,10 @@ var app = angular.module('decisionApp', ['ngRoute']);
 app.controller("decisionController", ['$scope', function($scope) {
   $scope.view = {};
   $scope.view.inputLocation = true;
+
+  $scope.view.changeInput = function(){
+    $scope.view.inputLocation = false;
+    $scope.view.inputTypeOfFood = true;
+  }
+  
 }])
