603baa88fe5f0400c7ad5895df2c06da60e49993
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-08-29 12:37:07 -0600

    Switched from using Materialize to Bootstrap

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,18 @@
 var app = angular.module('decisionApp', ['ngRoute']);
 
+app.controller("DecisionController", ['$scope', function($scope) {
+  $scope.view = {};
+  $scope.view.inputLocation = true;
+}]);
+
 app.config(function($routeProvider){
   $routeProvider
     .when('/', {
-      templateUrl: 'views/home.html'
+      templateUrl: 'views/home.html',
+      controller: "DecisionController"
     })
     .when('/decide', {
-      templateUrl: 'views/search.html'
+      templateUrl: 'views/search.html',
+      controller: "DecisionController"
     })
-})
+});
