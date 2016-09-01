47fac08b672e4c466d7e59a5fe997f89e69e75b1
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-09-01 11:12:49 -0600

    made yelp search work and edited forms

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,16 @@ var express = require('express');
 var router = express.Router();
 var oauthSignature = require('oauth-signature');
 var yelp = require("node-yelp");
+var knex = require("knex");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-//
-// router.get('/api/yelp', function(req, res, next) {
-//   console.log("GET YELP");
-//   var yelps = {help: ['help', 'me']}
-//   res.json(yelps);
-// // });
 
-router.get('/api/yelp', function(req, res, next) {
+router.get('/api/yelp/:latitude/:longitude', function(req, res, next) {
+  var latitude = req.params.latitude;
+  var longitude = req.params.longitude;
   var client = yelp.createClient({
     oauth: {
       "consumer_key": process.env.oauth_consumer_key,
@@ -31,7 +28,36 @@ router.get('/api/yelp', function(req, res, next) {
   });
   client.search({
     terms: "food",
-    location: "Denver"
+    // location: "Denver"
+    cll: latitude,longitude
+  }).then(function (data) {
+    var businesses = data.businesses;
+    var location = data.region;
+    res.json(data);
+    // ...
+  }).catch(function(error) {
+    console.log(error);
+  })
+})
+
+router.get('/api/yelp/:city', function(req, res, next) {
+  var city = req.params.city;
+  var client = yelp.createClient({
+    oauth: {
+      "consumer_key": process.env.oauth_consumer_key,
+      "consumer_secret": process.env.consumerSecret,
+      "token": process.env.oauth_token,
+      "token_secret": process.env.tokenSecret
+    },
+
+    // Optional settings:
+    httpClient: {
+      maxSockets: 25  // ~> Default is 10
+    }
+  });
+  client.search({
+    terms: "restaurants",
+    location: city
   }).then(function (data) {
     var businesses = data.businesses;
     var location = data.region;
