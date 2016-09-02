01a416d95c6d3a498dacb79885c1085994eb7aeb
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-09-02 13:25:49 -0600

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,10 @@ module.exports = {
   development: {
     client: 'postgres',
     connection: 'postgres://localhost:5432/foodme'
+  },
+  production: {
+    client: 'postgres',
+    connection: process.env.DATABASE_URL
   }
 
 };
